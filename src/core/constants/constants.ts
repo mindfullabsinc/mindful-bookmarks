b4eafaa5192628d6b18ac5b117380986a1c2a4b7
abearman <amylbearman@gmail.com>
2025-11-17 16:46:33 -0800

    Added clickable browser icons for Chrome and Brave, as well as conditional logic to change the text of the CTA download button

--- src/core/constants/constants.ts
+++ src/core/constants/constants.ts
@@ -1,4 +1,5 @@
 /* --------------------Constants -------------------- */
+export const CHROME_EXTENSION_URL = "https://chromewebstore.google.com/detail/mindful/bjobloafhnodgomnplkfhebkihnafhfe";
 export const CHROME_NEW_TAB = "chrome://newtab/" as const;
 export const URL_PATTERN = "^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$" as const;
 export const EMPTY_GROUP_IDENTIFIER = "EMPTY_GROUP_IDENTIFIER" as const;
